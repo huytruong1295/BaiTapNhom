8cac02e0cb8b4d65df784b6686eb346b35ef602b
Le Truong Huy <huytruong1295@gmail.com>
2016-12-07 17:31:25 +0700

    Chuong trinh dau tien cua nhom !

---
+++ ChuyenBay/QL ChuyenBay/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace QL_ChuyenBay
+{
+    public partial class Form1 : Form
+    {
+        string cnstr;
+        SqlConnection cn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Connect()
+        {
+            cn.Open();
+
+        }
+        private void Disconnect()
+        {
+            cn.Close();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            // TEST FUNCTION WITH GITHUB
+            
+        }
+
+
+    }
+}
